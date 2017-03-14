cb3203e36086d79bf23432ef388f3ccb2cafe77d
Mykhailo Rybak <mykhailo.rybak.if@gmail.com>
2017-03-14 16:27:22 +0100

    countdown_champ: up to the end

--- countdown_champ/src/app.jsx
+++ countdown_champ/src/app.jsx
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
+import Clock from './clock';
 import './app.css';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2017',
+      newDeadline: ''
+    };
+  }
+
+  changeDeadline(){
+    this.setState({deadline: this.state.newDeadline})
+  }
+
   render(){
     return(
       <div className="app">
-        <div className='app-title'> Countdown to december 25, 2017 </div>
-        <div>
-          <div className='clock-days'> 14 days </div>
-          <div className='clock-hours'> 30 hours </div>
-          <div className='clock-minutes'> 15 minutes </div>
-          <div className='clock-seconds'> 20 seconds </div>
+        <div className='app-title'>
+          Countdown to {this.state.deadline}
         </div>
         <div>
-          <input placeholder='new date' />
-          <button> Submit </button>
+          <Clock
+            deadline={this.state.deadline}
+          />
         </div>
+        <Form inline={true}>
+          <FormControl
+            className='Deadline-input'
+            placeholder='new date'
+            onChange={event => this.setState({newDeadline: event.target.value})}
+          />
+          <Button onClick={() => this.changeDeadline()}>
+            Submit
+          </Button>
+        </Form>
       </div>
     )
   }
