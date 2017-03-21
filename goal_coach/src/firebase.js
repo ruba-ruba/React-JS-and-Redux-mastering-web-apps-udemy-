77901f148ec26f9b35058a10c7b54daae39a2ac7
Mykhailo Rybak <mykhailo.rybak.if@gmail.com>
2017-03-21 10:53:51 +0100

    goal_coach: added firebase

---
+++ goal_coach/src/firebase.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyAaWUDFiE8aow7N3ILqJj89pgwUUbb_vpo",
+  authDomain: "goal-coach-56de1.firebaseapp.com",
+  databaseURL: "https://goal-coach-56de1.firebaseio.com",
+  storageBucket: "goal-coach-56de1.appspot.com",
+  messagingSenderId: "549724708453"
+};
+
+export const firebaseApp = firebase.initializeApp(config);
--- goal_coach/src/index.js
+++ goal_coach/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
+import { firebaseApp } from './firebase';
 
 import App from './components/app';
 import SignIn from './components/sign_in';
 import SignUp from './components/sign_up';
+
+firebaseApp.auth().onAuthStateChanged(user => {
+  if (user){
+    console.log('user has signed in or up');
+  } else {
+    console.log('not authorized user');
+  }
+})
 
 ReactDOM.render(
   <Router path="/" history={browserHistory}>
