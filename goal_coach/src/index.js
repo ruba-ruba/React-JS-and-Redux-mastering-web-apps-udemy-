62f5aed2b8201a261b0fc7cfc40de3bac08fcb5c
Mykhailo Rybak <mykhailo.rybak.if@gmail.com>
2017-03-21 10:13:15 +0100

    goal_coach: fixed react-router

--- goal_coach/src/index.js
+++ goal_coach/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+
 import App from './components/app';
 import SignIn from './components/sign_in';
 import SignUp from './components/sign_up';
 
 ReactDOM.render(
-  <div>APP</div>, document.getElementById('root')
+  <Router path="/" history={browserHistory}>
+    <Route path="/app" component={App} />
+    <Route path="/sign_in" component={SignIn} />
+    <Route path="/sign_up" component={SignUp} />
+  </Router>, document.getElementById('root')
 )
