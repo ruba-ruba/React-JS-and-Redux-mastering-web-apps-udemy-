e38348f5d8e5944c91b57e456e6b238ed67fe7f5
Mykhailo Rybak <mykhailo.rybak.if@gmail.com>
2017-03-21 09:39:16 +0100

    goal_coach: setup app

---
+++ goal_coach/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import SignIn from './components/sign_in';
+import SignUp from './components/sign_up';
+
+ReactDOM.render(
+  <div>APP</div>, document.getElementById('root')
+)
